fbe647baa8e5220b19de48090051139cbad00df8
DeRey77 <johndoe@example.com>
2024-02-26 20:18:33 -0300

    Subiendo repositorio

--- Desktop/TrabajoFinalDS/src/componentes/VentanaInicioSesion.jsx
+++ Desktop/TrabajoFinalDS/src/componentes/VentanaInicioSesion.jsx
@@ -2,29 +2,31 @@ import React, { useState, useEffect } from 'react';
 import Cabecera from './Cabecera';
 import PieDePagina from './PieDePagina';
 
+// Importa las imágenes utilizando rutas relativas
+import img1 from './img1.jpeg';
+import img2 from './img2.jpeg';
+import img3 from './img3.jpeg';
 
 const VentanaInicioSesion = ({ onIniciarSesion }) => {
-  const [imagenIndex, setImagenIndex] = useState(0); // Estado para el índice de la imagen actual
+  const [imagenIndex, setImagenIndex] = useState(0);
 
   useEffect(() => {
-    // Función para cambiar la imagen cada 10 segundos
     const interval = setInterval(() => {
-      setImagenIndex((prevIndex) => (prevIndex + 1) % 3); // Cambia al siguiente índice de imagen
-    }, 10000); // 10000 milisegundos = 10 segundos
+      setImagenIndex((prevIndex) => (prevIndex + 1) % 3);
+    }, 6000);
 
-    return () => clearInterval(interval); // Limpia el intervalo cuando el componente se desmonta
-  }, []); // El efecto solo se ejecuta una vez al montar el componente
+    return () => clearInterval(interval);
+  }, []);
 
-  const imagenes = ['img1.jpeg', 'img2.jpeg', 'img3.jpeg']; // Nombres de las imágenes
+  const imagenes = [img1, img2, img3]; // Utiliza las variables de imagen importadas
 
   const handleIniciarSesionClick = () => {
-    onIniciarSesion(); // Llama a la función para iniciar sesión
+    onIniciarSesion();
   };
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
       <Cabecera onCerrarSesion={handleIniciarSesionClick} />
-      {/* Renderiza la imagen actual */}
       <img src={imagenes[imagenIndex]} alt="imagen" style={{ maxWidth: '100%', maxHeight: '100%' }} />
       <PieDePagina valorDolar={100} clima="Soleado" datoAdicional="Sin demoras en las calles" />
     </div>
